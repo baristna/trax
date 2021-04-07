cf8520e9005c921864cd89f74728aba9f20c625c
Barış Tuna <baristna@gmail.com>
2021-04-07 05:05:30 +0300

    move from bitbucket

---
+++ Assets/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("m^s-1")] public float xSpeed = 5f;
+    [Tooltip("m^s-1")] public float ySpeed = 5f;
+
+    public float xRange = 8f;
+    public float yRange = 4f;
+    public float pitchFactor = -2f;
+    public float yawFactor = -2f;
+
+    public float controlPitchFactor = -20f;
+
+    float xThrow = 0f;
+    float yThrow = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Translation();
+        Rotation();
+    }
+
+    void Rotation()
+    {
+        float pitch = transform.localPosition.y * pitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * -yawFactor - xThrow * controlPitchFactor; // transform.localPosition.x * -pitchFactor;
+        float roll = xThrow * controlPitchFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void Translation() {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float newX = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
+        float newY = Mathf.Clamp(transform.localPosition.y + yOffset, -yRange, yRange);
+        transform.localPosition = new Vector3(newX, newY, transform.localPosition.z);
+
+        // transform.eulerAngles = new Vector3(
+        //     transform.eulerAngles.x,
+        //     transform.eulerAngles.y,
+        //     -xThrow * 30
+        // );
+    }
+}
---
+++ Assets/pg_ship.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pg_ship : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidBody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(new Vector3(0, 20, 0));
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(new Vector3(0, 0, -5));
+        } else if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(new Vector3(0, 0, 5));
+        } else {
+            if ((transform.eulerAngles.z < 5) || (transform.eulerAngles.z > 355) ) {
+                print(transform.eulerAngles.z);
+                transform.Rotate(0,0,0);
+            } else {
+                print("fix");
+                if (transform.eulerAngles.z > 0 && transform.eulerAngles.z <= 180 ) {
+                    transform.Rotate(new Vector3(0, 0, -1));
+                } else {
+                    transform.Rotate(new Vector3(0, 0, 1));
+                }
+            }
+        }
+    }
+}
